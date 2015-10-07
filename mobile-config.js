de6294a8a7d84abf1b2c39fc5c08491bdcdad242
Max Tilford <maxtilford@gmail.com>
2015-10-07 16:13:50 -0700

    adds ios icons and splash screens

---
+++ mobile-config.js
@@ -0,0 +1,21 @@
+App.info({
+  id: 'com.likeus',
+  name: 'LikeUs',
+  description: 'An iOS app built with Meteor',
+  version: '0.0.1'
+});
+
+App.icons({
+  'iphone': 'resources/icons/Icon.png',
+  'iphone_2x': 'resources/icons/Icon-60@2x.png',
+	'iphone_3x': 'resources/icons/Icon-60@3x.png',
+	'ipad': 'resources/icons/Icon-76.png',
+	'ipad_2x': 'resources/icons/Icon-76@2x.png'
+});
+
+App.launchScreens({
+  'iphone': 'resources/splash/242x480.png',
+  'iphone5': 'resources/splash/242x480.png',
+  'iphone6': 'resources/splash/242x480.png',
+  'iphone6p_portrait': 'resources/splash/242x480.png'	
+});
