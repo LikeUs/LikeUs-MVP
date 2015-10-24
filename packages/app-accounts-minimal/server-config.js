1bbc7651231dbd19c459111d2abc51cb821d6177
Max Tilford <maxtilford@gmail.com>
2015-10-24 06:26:09 -0700

    adds phone verification

--- packages/app-accounts-minimal/server-config.js
+++ packages/app-accounts-minimal/server-config.js
@@ -1,2 +1,17 @@
 
 AccountsGuest.anonymous = true
+Meteor.users.allow({
+  update: function(userId, doc, fieldNames, modifier) {
+    return (userId && doc._id === userId);
+  }
+})
+
+
+Meteor.publish("userData", function () {
+  if (this.userId) {
+    return Meteor.users.find({_id: this.userId},
+                             {fields: {'phone': 1}});
+  } else {
+    this.ready();
+  }
+});
--- packages/app-onboarding/onboarding.js
+++ packages/app-onboarding/onboarding.js
@@ -1,22 +1,24 @@
 
-
-var OnboardingContinue = function(viewport) {
+var runTemplate = function(template, viewport) {
   var deferred = Q.defer();
 
-  viewport.goTo("OnboardingContinue", { data: { deferred: deferred } });
+  viewport.goTo(template, { data: { deferred: deferred } });
 
   return deferred.promise;
 };
-
 
 RunOnboarding = function(quiz, viewport) {
 
   return TakeQuiz(quiz, viewport)
     .then(function() {
+      return runTemplate("OnboardingContinue", viewport);
 
-      viewport.goTo("OnboardingContinue");
-      return OnboardingContinue(viewport);
-
+    })
+    .then(function() {
+      return runTemplate("OnboardingGetPhone", viewport);
+    })
+    .then(function() {
+      return runTemplate("OnboardingVerifyPhone", viewport);
     })
     .then(function() {
       return ComingSoon(viewport);
@@ -27,4 +29,45 @@ RunOnboarding = function(quiz, viewport) {
 
 Template.OnboardingContinue.events({
   "click [rel=continue]": function() { Template.currentData().deferred.resolve(); }
+});
+
+Template.OnboardingGetPhone.events({
+  "submit form": function(e) {
+    e.preventDefault();
+    var phone = e.currentTarget.phone.value;
+    console.log('foo');
+
+    var deferred = Template.currentData().deferred;
+
+    Meteor.users.update(Meteor.userId(), {
+      $set: {
+        phone: phone
+      }
+    }, function(err) {
+      if (err) {
+        console.log(err);
+        deferred.reject(err);
+      } else {
+        Meteor.sendVerificationCode(phone);
+        deferred.resolve();
+      }
+    });
+  }
+});
+
+Template.OnboardingVerifyPhone.events({
+  "submit form": function(e) {
+    e.preventDefault();
+    var code = e.currentTarget.code.value;
+    var deferred = Template.currentData().deferred;
+    Meteor.loginWithSms(Meteor.user().phone, code, function(err) {
+      if (err) {
+        console.log(err);
+        deferred.reject(err);
+      } else {
+        deferred.resolve();
+      }
+    });
+
+  }
 });
